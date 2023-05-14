005997bdaec82f2c272f924f3b38316a028a8ecc
matibe95 <matibel2005@gmail.com>
2023-05-14 20:43:13 -0300

    crear endpoint para resultados

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const axios = require('axios')
 const cheerio = require('cheerio')
 const app = require('express')()
 const port = process.env.PORT || 8008
-const { convertArraysIntoProperObject, splitArrays } = require('./cleanDataFunctions.js')
-const { DEFAULT_SERIE, SERIES } = require('./constants/webUrls.js')
+const { convertArraysIntoProperObject, splitArrays, arrayToObjectSerie_Results } = require('./cleanDataFunctions.js')
+const { DEFAULT_SERIE, SERIES, RESULTADOS } = require('./constants/webUrls.js')
 
 app.get('/', (req, res) => {
   res.status(200).send('<h1>Matibe LUD Data Api</h1>')
@@ -20,10 +20,36 @@ app.get('/serie/:id', async (req, res) => {
     const $ = cheerio.load(data)
 
     const info = $('div table tbody tr td').get().map(val => $(val).text())
-    const infoSplit = splitArrays(info)
+    const infoSplit = splitArrays(info, 8)
     const dataObject = convertArraysIntoProperObject(infoSplit)
 
-    res.send({ data: dataObject })
+    res.send({
+      tabla: dataObject
+    })
+
+  } catch (error) {
+    res.status(500).send({ message: error.message })
+  }
+
+})
+app.get('/serie/:id/resultados/:resultados', async (req, res) => {
+
+  const { id } = req.params;
+  const { resultados } = req.params;
+
+  try {
+    const url = RESULTADOS[resultados] || DEFAULT_SERIE
+
+    const { data } = await axios.get(url)
+    const $ = cheerio.load(data)
+
+    const info = $('div table tbody tr td').get().map(val => $(val).text())
+    const infoSplit = splitArrays(info, 6)
+    const dataObject = arrayToObjectSerie_Results(infoSplit)
+
+    res.send({
+      resultados: dataObject
+    })
 
   } catch (error) {
     res.status(500).send({ message: error.message })
