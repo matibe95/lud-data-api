ee1698bf20d4029610cef8f0912387cc109a97b7
matibe95 <matibel2005@gmail.com>
2023-05-14 19:58:51 -0300

    creacion de constantes para las urls de las series

---
+++ constants/webUrls.js
@@ -0,0 +1,14 @@
+const DEFAULT_SERIE = 1
+
+const SERIES = Object.freeze({
+  1: 'http://box2023.temp.domains/~ligauniv/get.php?link=1Posiciones_red.asp?temporada=110!deporte=F!torneo=18!categoria=18!rueda=1!serie=18-1-',
+  2: 'http://box2023.temp.domains/~ligauniv/get.php?link=1Posiciones_red.asp?temporada=110!deporte=F!torneo=18!categoria=18!rueda=1!serie=18-2-',
+  3: 'http://box2023.temp.domains/~ligauniv/get.php?link=1Posiciones_red.asp?temporada=110!deporte=F!torneo=18!categoria=18!rueda=1!serie=18-3-',
+  4: 'http://box2023.temp.domains/~ligauniv/get.php?link=1Posiciones_red.asp?temporada=110!deporte=F!torneo=18!categoria=18!rueda=1!serie=18-4-',
+  5: 'http://box2023.temp.domains/~ligauniv/get.php?link=1Posiciones_red.asp?temporada=110!deporte=F!torneo=18!categoria=18!rueda=1!serie=18-5-',
+})
+
+module.exports = {
+  SERIES,
+  DEFAULT_SERIE,
+}
