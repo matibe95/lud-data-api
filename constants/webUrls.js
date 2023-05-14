eb78c75408dc27fafa6c3ab37ca32d241306f01c
matibe95 <matibel2005@gmail.com>
2023-05-14 20:43:28 -0300

    agregar urls para seccion resultados

--- constants/webUrls.js
+++ constants/webUrls.js
@@ -8,7 +8,16 @@ const SERIES = Object.freeze({
   5: 'http://box2023.temp.domains/~ligauniv/get.php?link=1Posiciones_red.asp?temporada=110!deporte=F!torneo=18!categoria=18!rueda=1!serie=18-5-',
 })
 
+const RESULTADOS = Object.freeze({
+  1: 'http://box2023.temp.domains/~ligauniv/get.php?link=3Fixture_red.asp?temporada=110!deporte=F!torneo=18!categoria=18!rueda=1!serie=18-1-!filtrarFecha=0',
+  2: 'http://box2023.temp.domains/~ligauniv/get.php?link=3Fixture_red.asp?temporada=110!deporte=F!torneo=18!categoria=18!rueda=1!serie=18-2-!filtrarFecha=0',
+  3: 'http://box2023.temp.domains/~ligauniv/get.php?link=3Fixture_red.asp?temporada=110!deporte=F!torneo=18!categoria=18!rueda=1!serie=18-3-!filtrarFecha=0',
+  4: 'http://box2023.temp.domains/~ligauniv/get.php?link=3Fixture_red.asp?temporada=110!deporte=F!torneo=18!categoria=18!rueda=1!serie=18-4-!filtrarFecha=0',
+  5: 'http://box2023.temp.domains/~ligauniv/get.php?link=3Fixture_red.asp?temporada=110!deporte=F!torneo=18!categoria=18!rueda=1!serie=18-5-!filtrarFecha=0',
+})
+
 module.exports = {
   SERIES,
   DEFAULT_SERIE,
+  RESULTADOS
 }
