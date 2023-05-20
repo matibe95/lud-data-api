c107b1c81a3156adc695b24aba3d58dd3cdfa4a9
matibe95 <matibel2005@gmail.com>
2023-05-20 19:37:14 -0300

    limpiar código en la parte de traer la tabla. En vez de traer una serie sola se puede elegir y traer todas

--- index.js
+++ index.js
@@ -3,23 +3,38 @@ const cheerio = require('cheerio')
 const cors = require('cors')
 const app = require('express')()
 const port = process.env.PORT || 8008
-const { convertArraysIntoProperObject, splitArrays, arrayToObjectSerie_Results } = require('./src/functions/cleanDataFunctions.js')
-const { DEFAULT_SERIE, SERIES, RESULTADOS } = require('./src/constants/webUrls.js')
+const { convertArraysIntoProperObject, splitArrays, arrayToObjectSerie_Results, convertSerieArrayIntoObject } = require('./src/functions/cleanDataFunctions.js')
+const { DEFAULT_SERIE, URL_SERIES, RESULTADOS } = require('./src/constants/webUrls.js')
 const { htmlPage } = require('./src/constants/htmlPage.js')
 
 app.use(cors())
 
 app.get('/', (req, res) => {
-
   res.status(200).send(htmlPage)
 })
 
-app.get('/serie/:id', async (req, res) => {
+app.get('/serie/:id?*', async (req, res) => {
 
   const { id } = req.params;
 
   try {
-    const url = SERIES[id] || DEFAULT_SERIE
+    async function traerTabla() {
+      if (!!id) {
+        return await tablaDeUnaSolaSerie({ numSerie: id })
+      }
+      return await tablaDeTodasLasSeries()
+    }
+
+    res.send({
+      data: await traerTabla()
+    })
+
+  } catch (error) {
+    res.status(500).send({ message: error.message })
+  }
+
+  async function tablaDeUnaSolaSerie({ numSerie }) {
+    const url = URL_SERIES[numSerie]
 
     const { data } = await axios.get(url)
     const $ = cheerio.load(data)
@@ -28,15 +43,19 @@ app.get('/serie/:id', async (req, res) => {
     const infoSplit = splitArrays(info, 8)
     const dataObject = convertArraysIntoProperObject(infoSplit)
 
-    res.send({
-      data: dataObject
-    })
+    return dataObject
+  }
 
-  } catch (error) {
-    res.status(500).send({ message: error.message })
+  async function tablaDeTodasLasSeries() {
+    let arrayRespuesta = []
+    for (const i in URL_SERIES) {
+      arrayRespuesta.push(await tablaDeUnaSolaSerie({ numSerie: i }))
+    }
+    return convertSerieArrayIntoObject(arrayRespuesta)
   }
 
 })
+
 app.get('/serie/:id/resultados', async (req, res) => {
 
   const { id } = req.params;
