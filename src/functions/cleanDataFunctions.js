47c59bbcce08acb165570ee793b2d142c9e5b96f
matibe95 <matibel2005@gmail.com>
2023-05-26 00:06:56 -0300

    hacer funcion universal para todas las secciones. Modularizando el código

--- src/functions/cleanDataFunctions.js
+++ src/functions/cleanDataFunctions.js
@@ -7,23 +7,6 @@ const splitArrays = (initialArr, amount) => {
   return subArrays
 }
 
-const convertArraysIntoProperObject = (data) => {
-  const objetosArray = [];
-  for (const array of data) {
-    const objeto = {};
-    objeto.equipo = array[0];
-    objeto.jugados = array[1].trim();
-    objeto.ganados = array[2].trim();
-    objeto.empatados = array[3].trim();
-    objeto.perdidos = array[4].trim();
-    objeto.golesFavor = array[5].trim();
-    objeto.golesContra = array[6].trim();
-    objeto.puntos = array[7].trim();
-    objetosArray.push(objeto);
-  }
-  return objetosArray
-}
-
 const convertSerieArrayIntoObject = (data) => {
   const objeto = {};
   objeto.serie1 = data[0]
@@ -35,23 +18,12 @@ const convertSerieArrayIntoObject = (data) => {
   return objeto
 }
 
-const arrayToObjectSerie_Results = (data) => {
-  const objetosArray = [];
-  for (const array of data) {
-    const objeto = {};
-
-    objeto.primerEquipo = array[2].trim();
-    objeto.primerEquipoGoles = array[3].trim().replace(/[\s\-]+/g, '');
-    objeto.segundoEquipoGoles = array[4].trim();
-    objeto.segundoEquipo = array[5].trim();
-    objetosArray.push(objeto);
-  }
-  return objetosArray
+const convertirDataEnArrayDeObjetos = (data, accion, numSerie) => {
+  return (accion({ data, numSerie }))
 }
 
 module.exports = {
-  convertArraysIntoProperObject,
   splitArrays,
-  arrayToObjectSerie_Results,
+  convertirDataEnArrayDeObjetos,
   convertSerieArrayIntoObject,
 }
