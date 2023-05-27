29a90f88d8b7e8603ea7dc8327d457c574b4f08e
matibe95 <matibel2005@gmail.com>
2023-05-27 01:05:11 -0300

    eliminar funciones no utilizadas

--- src/functions/cleanDataFunctions.js
+++ src/functions/cleanDataFunctions.js
@@ -7,23 +7,23 @@ const splitArrays = (initialArr, amount) => {
   return subArrays
 }
 
-const convertSerieArrayIntoObject = (data) => {
-  const objeto = {};
-  objeto.serie1 = data[0]
-  objeto.serie2 = data[1]
-  objeto.serie3 = data[2]
-  objeto.serie4 = data[3]
-  objeto.serie5 = data[4]
+// const convertSerieArrayIntoObject = (data) => {
+//   const objeto = {};
+//   objeto.serie1 = data[0]
+//   objeto.serie2 = data[1]
+//   objeto.serie3 = data[2]
+//   objeto.serie4 = data[3]
+//   objeto.serie5 = data[4]
 
-  return objeto
-}
+//   return objeto
+// }
 
 const convertirDataEnArrayDeObjetos = (data, accion, numSerie) => {
-  return (accion({ data, numSerie }))
+  return accion({ data, numSerie })
 }
 
 module.exports = {
   splitArrays,
   convertirDataEnArrayDeObjetos,
-  convertSerieArrayIntoObject,
+  // convertSerieArrayIntoObject,
 }
