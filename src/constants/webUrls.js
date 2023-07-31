816797528739104c4121f40edc6501def851972a
matibe95 <matipro365@gmail.com>
2023-07-30 22:33:29 -0300

    crear constante con urls de las copas

--- src/constants/webUrls.js
+++ src/constants/webUrls.js
@@ -14,7 +14,23 @@ const RESULTADOS = Object.freeze({
   5: 'http://box2023.temp.domains/~ligauniv/get.php?link=3Fixture_red.asp?temporada=110!deporte=F!torneo=18!categoria=18!rueda=1!serie=18-5-!filtrarFecha=0',
 })
 
+const RESULTADOS_COPAS = Object.freeze({
+  'oro': 'http://box2023.temp.domains/~ligauniv/get.php?link=3Fixture_red.asp?temporada=110!deporte=F!torneo=18!categoria=18!rueda=2!serie=18o!filtrarFecha=0',
+  'plata': 'http://box2023.temp.domains/~ligauniv/get.php?link=3Fixture_red.asp?temporada=110!deporte=F!torneo=18!categoria=18!rueda=2!serie=18p!filtrarFecha=0',
+  'bronce': 'http://box2023.temp.domains/~ligauniv/get.php?link=3Fixture_red.asp?temporada=110!deporte=F!torneo=18!categoria=18!rueda=2!serie=18b!filtrarFecha=0',
+  'competencia': 'http://box2023.temp.domains/~ligauniv/get.php?link=3Fixture_red.asp?temporada=110!deporte=F!torneo=18!categoria=18!rueda=2!serie=18c!filtrarFecha=0',
+})
+
+const TABLA_COPAS = Object.freeze({
+  'oro': 'http://box2023.temp.domains/~ligauniv/get.php?link=1Posiciones_red.asp?temporada=110!deporte=F!torneo=18!categoria=18!rueda=2!serie=18o',
+  'plata': 'http://box2023.temp.domains/~ligauniv/get.php?link=1Posiciones_red.asp?temporada=110!deporte=F!torneo=18!categoria=18!rueda=2!serie=18p',
+  'bronce': 'http://box2023.temp.domains/~ligauniv/get.php?link=1Posiciones_red.asp?temporada=110!deporte=F!torneo=18!categoria=18!rueda=2!serie=18b',
+  'competencia': 'http://box2023.temp.domains/~ligauniv/get.php?link=1Posiciones_red.asp?temporada=110!deporte=F!torneo=18!categoria=18!rueda=2!serie=18c',
+})
+
 module.exports = {
   URL_SERIES,
-  RESULTADOS
+  RESULTADOS,
+  RESULTADOS_COPAS,
+  TABLA_COPAS
 }
