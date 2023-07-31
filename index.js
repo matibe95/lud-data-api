e5e6e51c427544e8e6274d93a388e4482102735b
matibe95 <matipro365@gmail.com>
2023-07-30 22:35:57 -0300

    agregar nueva ruta para las copas

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = require('express')()
 const port = process.env.PORT || 8008
 const { htmlPage } = require('./src/constants/htmlPage.js')
 const { traerDataDeLaPagina } = require('./src/functions/traerResultadosDeLaPagina.js')
+const { traerDataDeLaPagina_copas } = require('./src/functions/traerResultadosDeLaPagina_copas.js')
 
 app.use(cors())
 
@@ -24,5 +25,23 @@ app.get('/serie/:id/seccion/:seccion', async (req, res) => {
     res.status(500).send({ message: error.message })
   }
 })
+
+app.get('/v2/serie/:copa/seccion/:seccion', async (req, res) => {
+  const { copa, seccion } = req.params;
+  const seccionPágina = seccion
+  
+  try {
+    res.send({
+      data: await traerDataDeLaPagina_copas({copa, seccionPágina})
+    })
+  } catch (error) {
+    console.log(error)
+    res.status(500).send({ message: error.message })
+  }
+})
+
+process.on('uncaughtException', function (err) {
+  console.log(err);
+}); 
 
 app.listen(port, () => console.log(`Server iniciado correctamente en el puerto ${port}`))
